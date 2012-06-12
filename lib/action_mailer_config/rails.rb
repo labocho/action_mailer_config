09c44565d2936e729b35c72e92839f25a71fb9db
labocho <labocho@penguinlab.jp>
2012-06-11 19:34:09 -0700

    Fix typo / Ignore config missing

--- lib/action_mailer_config/rails.rb
+++ lib/action_mailer_config/rails.rb
@@ -2,7 +2,19 @@ require "action_mailer_config"
 require "yaml"
 
 path = "#{Rails.root}/config/mail.yml"
-raise "Could not found ActionMailer config file: #{path}" unless File.exists? path
-config_for_all = YAML.load_file(path)
-raise "Could not found ActionMailer config for #{Raile.env.inspect}" unless config_for_all.has_key? Rails.env
-ActionMailerConfig.load config_for_all(Rails.env)
+
+config = if File.exists? path
+  config_for_all = YAML.load_file(path)
+
+  if config_for_all.has_key? Rails.env
+    config_for_all[Rails.env]
+  else
+    warn "Could not found ActionMailer config for #{Rails.env.inspect}, use delivery_method: test"
+    {}
+  end
+else
+  warn "Could not found ActionMailer config file: #{path}, use delivery_method: test"
+  {}
+end
+
+ActionMailerConfig.load config
